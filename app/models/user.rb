775ed4453cad36c6de563df12067159793e6c579
noelfoka <nomgnenoel@gmail.com>
2023-11-17 21:55:14 +0100

    continius to setup devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
-    has_many :detail
-    has_many :tran_group
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         has_many :Details
+         has_many :TranGroups
+       
+         validates :name, presence: true
+         validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+         validates :password, presence: true, length: { minimum: 6 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  root to: "user#index"
-  resources :details
-  resources :tran_groups
-  resources :users
+  #devise_for :users
+  root 'splash#show'
+  devise_for :users
+  resources :users, only: [:index, :show, :new, :create]
+  resources :tran_groups, only: [:index, :show, :new, :create] do
+  resources :tran_details, only: [:index, :show, :new, :create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
