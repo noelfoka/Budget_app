43c5db3e9cc07fc0cbcfb7f5ea5a938b5a486e3d
noelfoka <nomgnenoel@gmail.com>
2023-11-20 07:55:31 +0100

    implemented navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action only: %i[ show edit update destroy ]
 
   # GET /users or /users.json
   def index
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
       end
     end
   end
-
+=begin
   # PATCH/PUT /users/1 or /users/1.json
   def update
     respond_to do |format|
@@ -58,16 +58,16 @@ class UsersController < ApplicationController
     @user.destroy!
 
     respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+      format.html { redirect_to new_user_session_path, notice: "User was successfully destroyed." }
       format.json { head :no_content }
     end
   end
-
+=end
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+    #def set_user
+     # @user = User.find(params[:id])
+    #end
 
     # Only allow a list of trusted parameters through.
     def user_params
